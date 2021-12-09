f2145c6b02b565c007bcd3f30e8c3192e51cafc6
Usman Shani <usman.shani@student.ap.be>
2021-12-09 15:43:22 +0100

    WIP Tickets Database

--- Code/MoneyTracker/src/main/java/HelperClass/ReadFromJSONFile.java
+++ Code/MoneyTracker/src/main/java/HelperClass/ReadFromJSONFile.java
@@ -1,13 +1,20 @@
 package HelperClass;
 
 import Database.PersonsDB;
+import Database.TicketsDB;
+import DatabaseController.PersonsDBController;
 import Model.Person;
+import Model.Ticket;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Month;
+import java.util.ArrayList;
+
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import Factory.*;
 
 public class ReadFromJSONFile {
     public static void readPersonFile(PersonsDB db){
@@ -19,7 +26,7 @@ public class ReadFromJSONFile {
             JSONArray personsList = (JSONArray) obj;
             System.out.println(personsList);
 
-            //Iterate over employee array
+            //Iterate over person array
             personsList.forEach(person -> parsePersonObject( (JSONObject) person , db) );
 
         }  catch (IOException e) {
@@ -52,5 +59,44 @@ public class ReadFromJSONFile {
         tmpPerson.setBirthDate(values[0], values[1], values[2]);
 
         db.add(tmpPerson);
+    }
+
+    public static void readTicketFile(TicketsDB db){
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader("./tickets.json")) {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray ticketsList = (JSONArray) obj;
+            System.out.println(ticketsList);
+
+            //Iterate over tickets array
+            ticketsList.forEach(ticket -> parseTicketObject( (JSONObject) ticket , db) );
+
+        }  catch (IOException e) {
+            System.err.printf("Unable to read file %s%s tickets.json\n", e);
+        }  catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parseTicketObject(JSONObject ticketObject, TicketsDB db) {
+        String payed_by = (String) ticketObject.get("Payed_by");
+
+        PersonsDB personsDB = PersonsDB.getInstance();
+        PersonsDBController personsDBController = new PersonsDBController(personsDB);
+        ArrayList<Person> personsList = personsDBController.getAll();
+        System.out.println("==========STARTING FOR LOOP=========");
+        for (int i = 0; i < personsList.size(); i++){
+            System.out.println("==========STARTING IF STATEMENT=========");
+            if (payed_by == personsList.get(i).toString()){
+                System.out.println("==========FOUND MATCH=========");
+            }
+        }
+
+        //create ticket
+        //Ticket ticket = TicketFactory.getTicket()
+
+        //db.add(tmpPerson);
     }
 }
